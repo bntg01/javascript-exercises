ea31ab2aca88bc9186b77df74f7a99161b8c841d
Ben <bntgion@gmail.com>
2022-09-21 16:58:12 -0400

    Add solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let strlen = string.length;
+
+    let strArray = Array.from(string);
+
+    const newStr = new Array(strlen);
+
+    for (let i = 0; i < strlen; i++) {
+        newStr.push(strArray.pop());
+    }
+
+    return newStr.join("");
 
 };
 
