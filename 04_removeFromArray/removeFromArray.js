c9d8a3b741f70b7a4a3d3dbbf92d18c0bb6740ad
Ben <bntgion@gmail.com>
2022-09-22 15:32:35 -0400

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,9 +3,11 @@ const removeFromArray = function(arr, ...args) {
         for (let j = 0; j < args.length; j++) {    
             if (arr[i]===args[j]) {
                 arr.splice(i, 1);
+                i--;
             }
         }
     }
+    return arr;
 };
 
 // Do not edit below this line
