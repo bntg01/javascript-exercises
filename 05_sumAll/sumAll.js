3f86b6065230237d8a38104ed8f6ce4b1dfbde69
Ben <bntgion@gmail.com>
2022-09-22 16:04:49 -0400

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numFirst, numLast) {
+    const arr = [];
+    if (typeof numFirst === "number" && typeof numLast === "number" && numFirst >= 0 && numLast >=0) {
+        if (numFirst < numLast) {        
+            for (let i = numFirst; i <= numLast; i++) {
+                arr.push(i);
+            }
+        }
+        else if(numFirst > numLast) {
+            for (let i = numFirst; i >= numLast; i--) {
+                arr.push(i);
+            }
+        }
+        return arr.reduce((a,b)=> a + b, 0);
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
