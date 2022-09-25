029e66e08e91b4eb762dc6d86b0c91b823e0bbf1
Ben <bntgion@gmail.com>
2022-09-25 18:53:42 -0400

    Add solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArr) {
+    let titleArray = [];
 
+    for(let i = 0; i <= objArr.length-1; i++) {
+        titleArray.push(objArr[i].title);
+    }
+    
+    return titleArray;
 };
 
 // Do not edit below this line
