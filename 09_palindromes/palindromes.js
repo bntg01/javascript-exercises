b68deaff09d2eb5162b36e690e53ff4465618f79
Ben <bntgion@gmail.com>
2022-09-25 18:23:03 -0400

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s\']|_/g, "")
+    string = string.replace(/\s/g, "");
+    string = string.toLowerCase();
+    var revArray = string.split("");
+    revArray.reverse();
+    var revString = revArray.join("");
+    if (string === revString) {
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
