7fbb134598d0e6543ee14300d4fa835cdcb60570
Ben <bntgion@gmail.com>
2022-09-25 18:36:49 -0400

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num > 0) {
+        num = Number(num);
+        let fibArray = [1, 1];
+        for (i = 0; i <= num; i++) {
+            let newNum = fibArray[i] + fibArray[i+1];
+            fibArray.push(newNum);
+        }
+        return fibArray[num-1];
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
